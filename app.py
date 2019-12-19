886e5ae7d6f48256e06859df4c16f1f6ca734761
myname21-meet <myname21@meet.mit.edu>
2019-12-19 11:40:24 -0500

    finished lab with all bonuses

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
+import requests
+import json
 from flask import Flask, render_template, request
 app = Flask(__name__)
+
+headers = {'Authorization': 'Key 91d90c19038d4629ad99b8e84c6df34f'}
 
 @app.route('/')
 def home():
@@ -9,14 +13,38 @@ def home():
 def study_image():
     
     image_url = request.form['url-input']
-    # At this point you have the image_url value from the front end
-    # Your job now is to send this information to the Clarifai API
-    # and read the result, make sure that you read and understand the
-    # example we covered in the slides! 
-
-    # YOUR CODE HERE!
     
-    return render_template('home.html', results="No results yet :(")
+    api_url = "https://api.clarifai.com/v2/models/aaa03c23b3724a16a56b629203edc62c/outputs"
+    
+    data ={"inputs": [
+      {
+        "data": {
+          "image": {
+            "url": image_url
+          }
+        }
+      }
+    ]}
+
+    response = requests.post(api_url, headers=headers, data=json.dumps(data))
+
+    info = json.loads(response.content)
+
+    infoo = info['outputs']
+    infod = infoo[0]['data']
+    infoc = infod['concepts']
+
+    hasHuman = False
+
+    for item in infoc:
+    	print('b')
+    	if item['name'] == 'people':
+    		print('a')
+    		hasHuman = True
+
+    firstItem = infoc[0]
+
+    return render_template('home.html', results=json.loads(response.content), hasHuman = hasHuman, main=firstItem['name'])
 
 if __name__ == '__main__':
     app.run(debug=True)
